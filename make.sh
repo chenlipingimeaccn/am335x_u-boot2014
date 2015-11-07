3f56a9875ee887b5390a62f9de16684086b3bae3
John.LiuXin <liuxing@embest-tech.com>
2015-11-07 14:03:51 +0800

    Init commit: uboot from rongdong, which release to jinrui
    Signed-off-by: John.LiuXin <liuxing@embest-tech.com>

---
+++ make.sh
@@ -0,0 +1,23 @@
+TARGEDIR="/home/chengpg/tftp"
+SRCIMG="u-boot.img MLO"
+
+make distclean
+
+make am335x_evm_nor_config
+
+make CROSS_COMPILE=arm-linux-
+
+echo "Info: COPY $SRCIMG -> $TARGEDIR"
+cp -rf $SRCIMG $TARGEDIR
+
+make distclean
+
+SRCIMG="u-boot.bin"
+DESTIMG="u-boot-nor.img"
+make am335x_evm_norboot_config
+
+make CROSS_COMPILE=arm-linux-
+
+echo "Info: COPY $SRCIMG -> $TARGEDIR/$DESTIMG"
+cp -rf $SRCIMG $TARGEDIR/$DESTIMG
+
